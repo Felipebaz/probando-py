dcf2dcf6bb86c0605fb54ee530f9e7942b2ee5aa
Felipe Baz <fbazlevrero@gmail.com>
2026-03-27 10:42:15 -0300

    test conditional

---
+++ conditional.py
@@ -0,0 +1,18 @@
+temperatura = 30
+
+if temperatura > 25 :
+    print ("Hace calor")
+    print ("Vamos a la playa")
+
+
+nota = input("Introduce tu nota: ") 
+if nota >= 90 :
+    print ("A (Sobresaliente)")
+elif nota >= 70 :
+    print ("B (Notable)")
+elif nota >=50 :
+    print ("C (Aprobado)")
+else :
+    print("D (Suspenso)")
+
+
