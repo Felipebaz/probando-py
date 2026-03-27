7a83b584c9b7a3c240559705bc335bfa8d9e8d5b
Felipe Baz <fbazlevrero@gmail.com>
2026-03-27 10:21:55 -0300

    test con inputs

---
+++ input.py
@@ -0,0 +1,8 @@
+edad = input ("Cual es tu edad? ")
+print (f"Tu edad es {edad}")
+
+
+edad_str = input ("Cual es tu edad? ")
+edad_int = int(edad_str)
+print (f"El proximo año tendrás {edad_int + 1} años")
+
