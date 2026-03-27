429f14d4d33481f209a13bb0bc665a8979a6b3e6
Felipe Baz <fbazlevrero@gmail.com>
2026-03-27 10:14:49 -0300

    test con strings

---
+++ strings.py
@@ -0,0 +1,8 @@
+rol = 'developer'
+empresa = 'Google'
+
+mensaje = "Mi rol es " + rol + " en " + empresa
+print (mensaje)
+
+mensaje_moderno = f"Mi rol es {rol} en {empresa}"
+print (f"El doble de 5 es {5*2}")
